7c24334860265414b932e96518ea68382ef76554
Jose Ricardo <ricardoingenierias@gmail.com>
2018-12-04 21:18:45 -0500

    se agregan estilos a la aplicación

--- src/pages/pendientes/pendientes.component.ts
+++ src/pages/pendientes/pendientes.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 import { ActividadesService } from "../../services/actividades.service";
+import { List } from "ionic-angular";
+import { Lista } from "../../models";
 
 @Component({
     selector: 'page-pendientes',
@@ -7,9 +9,13 @@ import { ActividadesService } from "../../services/actividades.service";
 })
 export class PendientesComponent {
 
-
+   
     constructor( private actividadesService: ActividadesService) {
+        
+    }
 
+    listaSelected(lista: Lista) {
+        console.log(lista);
     }
 
 }
--- src/pages/terminados/terminados.component.ts
+++ src/pages/terminados/terminados.component.ts
@@ -1,9 +1,20 @@
 import { Component } from "@angular/core";
+import { Lista } from "../../models";
+import { ActividadesService } from "../../services/actividades.service";
 
 @Component({
     selector: 'page-terminados',
     templateUrl: 'terminados.component.html'
 })
 export class TerminadosComponent {
+
+
+    constructor( private actividadesService: ActividadesService) {
+        
+    }
+
+    listaSelected(lista: Lista) {
+        console.log(lista);
+    }
 
 }
