cd1681c02891852e30b0cdb2c518891a3d307ac2
Jose Ricardo <ricardoingenierias@gmail.com>
2018-12-09 19:35:50 -0500

    se crea la funcion de editar nombre de una lista

--- src/components/lista/lista.component.ts
+++ src/components/lista/lista.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from "@angular/core";
 import { ActividadesService } from "../../services/actividades.service";
 import { Lista } from "../../models";
-import { NavController } from "ionic-angular";
+import { NavController, AlertController, ItemSliding } from "ionic-angular";
 import { AgregarComponent } from "../../pages/agregar/agregar.component";
 
 
@@ -15,7 +15,8 @@ export class ListaComponent {
 
     constructor( 
         private actividadesService: ActividadesService,
-        private navController: NavController) {
+        private navController: NavController,
+        private alertController: AlertController) {
         
     }
 
@@ -30,6 +31,36 @@ export class ListaComponent {
 
     borrarLista(lista: Lista) {
         this.actividadesService.borrarLista(lista);
+    }
+
+    editarLista(lista: Lista, itemSliding: ItemSliding) {
+        
+        itemSliding.close();
+        
+        const alerta = this.alertController.create({
+            title:'Editar',
+            message:'Editar nombre de la lista',
+            inputs:[{
+                name: 'titulo',
+                placeholder: 'Titulo de la lista',  
+                value: lista.titulo              
+            }],
+            buttons:[{
+                text: 'Cancelar',
+            }, {
+                text: 'Guardar',
+                handler: data => {
+
+                    if (data.titulo.length === 0 ) {
+                        return;
+                    }
+                    lista.titulo =  data.titulo;
+                    this.actividadesService.guardarStorage();
+                }
+            }]
+        });
+
+        alerta.present();
     }
 
 
